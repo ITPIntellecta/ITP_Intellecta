832a357d05bf5fd4229614de3dd78a25fba4d2a8
lenasaraba <lenasaraba8@gmail.com>
2024-03-22 17:37:00 +0100

    scroll

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function showList() {
-  const dropdown = document.getElementById("dropdown-list");
-  dropdown.classList.toggle("show-list");
-
-  const arrowDown = document.getElementsByClassName("span-arrow-down")[0];
-  arrowDown.classList.toggle("rotate-up");
-}
-
 function goToProfile() {
   location = "profile.html";
 }
@@ -24,3 +16,40 @@ function scrollL() {
   const el = document.getElementById("scroll");
   el.scrollBy({ left: -100, behavior: "smooth" });
 }
+
+document.addEventListener("click", function (event) {
+  const targetElement = document.getElementById("right-menu");
+  const tag = document.getElementById("nav-menu");
+  // Provera da li je kliknuti element različit od ciljnog elementa
+  if (event.target == targetElement) {
+    targetElement.classList.remove("show-menu");
+  } else if (event.target == tag) {
+    targetElement.classList.toggle("show-menu");
+  } else targetElement.classList.add("show-menu");
+
+  const targetEvr = document.getElementById("dropdown-list");
+  const header = document.getElementById("dropdown-header");
+  const arrowDown = document.getElementsByClassName("span-arrow-down")[0];
+  if (event.target != header) {
+    targetEvr.classList.remove("show-list");
+    arrowDown.classList.remove("rotate-up");
+  } else {
+    targetEvr.classList.toggle("show-list");
+    const arrowDown = document.getElementsByClassName("span-arrow-down")[0];
+    arrowDown.classList.toggle("rotate-up");
+  }
+});
+
+const scrollBtn = document.getElementById("scrollBtn");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scrollBtn.classList.remove("hidden");
+  } else {
+    scrollBtn.classList.add("hidden");
+  }
+});
+
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" }); // Skrolujte na vrh stranice glatko
+});
