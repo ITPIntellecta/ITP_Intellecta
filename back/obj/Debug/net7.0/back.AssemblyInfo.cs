301be35b64f5640c0a947a80db84dcdf0718aba2
jugovicivana <jugovicivana12@gmail.com>
2024-05-01 20:14:24 +0200

    oo

--- back/obj/Debug/net7.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net7.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e01d7475a07a85ba7fe53f6477547bd3cae9d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a88db860243e302d68b01b2f908530107362f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
