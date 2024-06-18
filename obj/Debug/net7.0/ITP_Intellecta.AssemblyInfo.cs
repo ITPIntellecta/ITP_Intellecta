6809eb5745a7ca779226ec3845b576e6009eeb5a
jugovicivana <jugovicivana12@gmail.com>
2024-06-18 15:14:39 +0200

    final

--- obj/Debug/net7.0/ITP_Intellecta.AssemblyInfo.cs
+++ obj/Debug/net7.0/ITP_Intellecta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITP_Intellecta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2aad7dd502ab235758e9e388c129c1e6e7a6145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e76ba645fa3778f181ad95ca0372d0ab873254")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITP_Intellecta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITP_Intellecta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
